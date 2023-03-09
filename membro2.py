ae4ea89d6fee2a4bd1dd0d140fd5174d8bae19aa
chocolatewithbanana <felipe.elton.ps@gmail.com>
2023-03-08 22:44:53 -0300

    Apresentação do Felipe

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou Felipe Elton, tenho 18 anos, gosto de jogar video-game. Eu aprendi programação em C no ensino médio no IFSC - Criciúma, e espero aprender como organizar o código de uma forma orientada a objetos"
