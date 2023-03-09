91e3657220e2f37d0c3672e66b760b91d9370118
Bianca <bi.mazzuco@gmail.com>
2023-03-09 20:46:39 -0300

    Apresentação da Bianca

--- membro4.py
+++ membro4.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro4" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou a Bianca Verzola, tenho 18 anos e gosto de patinação artística. Antes de começar o curso, eu não tinha nenhuma experiência com programação. Com essa disciplina espero aumentar meus conhecimento sobre programação." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
